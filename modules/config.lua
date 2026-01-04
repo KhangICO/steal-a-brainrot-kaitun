7e079f561dfefe2f8efcde95897372a5174e12d1
KhangICO <107622734+KhangICO@users.noreply.github.com>
2026-01-04 11:18:26 +0700

    Update config.lua

--- modules/config.lua
+++ modules/config.lua
@@ -1,1 +1,37 @@
+local Config = {
+    ["Gameplay"] = {
+        ["Server Type"] = "Private",
+        ["Collect Cash Cap"] = "10B",
+        ["Max Auctioning Multiplier"] = 10,
+        ["Lock Base Extra Time"] = 5,
+    },
 
+    ["Misc"] = {
+        ["Auto Buy"] = {
+            ["Enable"] = true,
+            ["Min Income Per Second"] = 1e7, -- 10M/s
+        },
+
+        -- CHỈ ghi tên Brainrot SECRET KHÔNG muốn mua
+        ["Ignore Secret"] = {
+            -- "Old Secret",
+            -- "Test Secret",
+        },
+
+        ["Kick if Ping above"] = 750,
+        ["Kick if FPS below"] = 5,
+        ["Max Rebirth"] = 0,
+    },
+
+    ["Performance"] = {
+        ["FPS Cap"] = 10,
+        ["Black Screen"] = true,
+    },
+
+    ["Webhook"] = {
+        ["Url"] = "",
+        ["Ignore Notify"] = {},
+    },
+}
+
+return Config
